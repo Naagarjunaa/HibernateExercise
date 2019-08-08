ee49259dc98ffce255f88003343801802c604cc3
Naagarjunaa <arjunravikkumar007@gmail.com>
2019-08-08 17:34:46 +0530

    second commit

--- JPAExercise/src/main/java/com/mycompany/hibernateexample/HelloWorld.java
+++ JPAExercise/src/main/java/com/mycompany/hibernateexample/HelloWorld.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.mycompany.hibernateexample;
 
 import javax.persistence.Column;
@@ -12,7 +8,7 @@ import javax.persistence.Table;
 
 /**
  *
- * @author aditi
+ * @author naagarjunaa
  */
 @Entity
 @Table(name = "hello")
--- JPAExercise/src/main/java/com/mycompany/hibernateexample/Main.java
+++ JPAExercise/src/main/java/com/mycompany/hibernateexample/Main.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.mycompany.hibernateexample;
 
 import javax.persistence.EntityManager;
@@ -11,7 +7,7 @@ import javax.persistence.Persistence;
 
 /**
  *
- * @author aditi
+ * @author naagarjunaa
  */
 
 public class Main {
